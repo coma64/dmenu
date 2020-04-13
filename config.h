de95339d91e1f15c40ef0443d025e45181bf8d7f
coma64 <coma64@outlook.com>
2020-04-13 04:07:03 +0200

    Change default colors to dracula like ones

--- config.h
+++ config.h
@@ -5,16 +5,16 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"DejaVu Sans Mono for Powerline:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+/*                                 fg         bg       */
+	[SchemeNorm]            = { "#f8f8f8", "#282a36" },
+	[SchemeSel]             = { "#f8f8f8", "#bd93f9" },
+	[SchemeSelHighlight]    = { "#00ff41", "#bd93f9" },
+	[SchemeNormHighlight]   = { "#282a36", "#50fa7b" },
+	[SchemeOut]             = { "#ff0000", "#ff0000" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 12;
@@ -26,4 +26,4 @@ static unsigned int lines      = 12;
 static const char worddelimiters[] = " -";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 3;
